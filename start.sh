17bf50f88240fa12bc08899c219b66267b90f3d5
ds <ds@gr.re>
2025-03-08 12:48:28 +0700

    ưe

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-echo "Starting MySQL..."
+echo "Starting mysqld..."
 /usr/local/bin/docker-entrypoint.sh mysqld &
 
+echo "Starting fpm8..."
+php-fpm8.3 -F &
+
+echo "Starting nginx..."
 nginx -g "daemon off;" &
 
-php-fpm8.3 -F &
+
 
 #php artisan serve --host=0.0.0.0 --port=7860 > /dev/null 2>&1
